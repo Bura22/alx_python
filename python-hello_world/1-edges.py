b8616363c5083a24edd2daefcbc91dfb7620fb1f
Biruk <Biruk Saleamlak>
2023-07-15 22:47:07 -0700

    slices updated

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,4 +3,4 @@ word = "Holberton"
 #the code goes here
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[7:9]))
-print("Middle word: {}".format(word[3:7]))
+print("Middle word: {}".format(word[1:8]))

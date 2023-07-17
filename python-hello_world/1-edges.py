aff4cc3844afd16f5297c703fb977178a1048366
Biruk <Biruk Saleamlak>
2023-07-17 12:31:28 -0700

    formatter updated

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 word = "Holberton"
-#the code goes here
+
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[7:9]))
 print("Middle word: {}".format(word[1:8]))

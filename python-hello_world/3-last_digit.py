9fc4198e8a7fdc0b79ae64a09e6347a6a1919410
Biruk <Biruk Saleamlak>
2023-07-17 11:47:17 -0700

    check the last digit of the number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,9 +1,15 @@
 import random
 
-number = random.randint(-100, 100)
+number = random.randint(-10000, 10000)
+new = 1
 
-lastDigit = number % 10
+if number < 0:
+    new = number * -1
+    lastDigit = new % 10
+else:
+    lastDigit = new % 10
 
+print(number, lastDigit)
 if lastDigit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
 else:

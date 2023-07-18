fcafca2c9e25a91a6558e5cbf7bb3cddec2a3e22
@Biruk <burasaleamlak24@gmail.com>
2023-07-18 17:00:19 +0400

    fibonacci series

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+def fibonacci_sequence(n):
+    a, b = 0, 1
+    c = 0
+    for i in range(0, n):
+        print(c, end=" ")
+        a = b
+        b = c
+        c = a + b
+number  = int(input("Enter number: "))
+fibonacci_sequence(number)

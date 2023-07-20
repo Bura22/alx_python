21ed0c9129048008d0778948cd406146c4757488
Biruk Saleamlak <113766296+Bura22@users.noreply.github.com>
2023-07-20 22:55:54 +0400

    fibonacci sequence

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,8 +4,14 @@ def fibonacci_sequence(n):
     a = 0
     b = 1
     series =[]
-    series.append(a)
-    series.append(b)
+    if n == 1:
+        series.append(a)
+    if n >= 2:
+        series.append(a)
+        series.append(b)
+    if n == 2:
+        series.append(a)
+        series.append(b)
     while (n > 2):
         allTotal = a + b
         series.append(allTotal)

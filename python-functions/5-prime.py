dee7e7195eba2ca3a6cb0f9a34c6e2bea93eadc8
Biruk Saleamlak <113766296+Bura22@users.noreply.github.com>
2023-07-20 22:01:18 +0400

    check if the number is prime or not

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -3,16 +3,13 @@
 def is_prime(number):
     if number == 0:
             return False
-            return False
     elif number < 0:
-            return False
             return False
     for i in range(2, number):
             
         if number % i == 0:
             return False
             break
-
     return  True
 
 

b0e23d8e54b9dc7fa259865198a69742fc4c5084
@Biruk <burasaleamlak24@gmail.com>
2023-07-20 15:42:50 +0400

    reverse string

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -2,6 +2,3 @@
 def reverse_string(string):
     reversed_string = string[::-1]
     return reversed_string
-
-string = input("Enter a string: ")
-print("THe reversed form of {} is {}".format(string, reverse_string(string)))
